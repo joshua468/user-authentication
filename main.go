f0dc3896bbb1ebd7739768ebb5fbcd9f6fbeae26
joshua468 <adegbesanjoshua9@gmail.com>
2024-07-09 14:56:57 +0100

    Update all to Vercel

--- main.go
+++ main.go
@@ -4,7 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/joshua468/user-authentication/config"
+	"github.com/joshua468/user-authentication/controllers"
+	"github.com/joshua468/user-authentication/middlewares"
+	"github.com/joshua468/user-authentication/models"
+)
+
+var (
+	db  *gorm.DB
+	cfg config.Config
 )
 
 func init() {
@@ -12,15 +25,53 @@ func init() {
 	if err := godotenv.Load("./config/.env"); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
+
+	// Load configuration
+	cfg = config.LoadConfig()
+
+	// Database connection
+	dsn := "host=" + cfg.DBHost + " user=" + cfg.DBUser + " password=" + cfg.DBPassword + " dbname=" + cfg.DBName + " port=5432 sslmode=disable"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&models.User{}, &models.Organisation{})
 }
 
 // Handler is the exported function for Vercel
 func Handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello, World!"))
-}
+	// Set up Gin
+	router := gin.Default()
 
-func main() {
-	http.HandleFunc("/", Handler)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	// Initialize controllers
+	authController := controllers.NewAuthController(db, cfg.JWTSecret)
+	orgController := controllers.NewOrganisationController(db)
+	userController := controllers.NewUserController(db)
+
+	// Routes
+	api := router.Group("/api")
+	{
+		authRoutes := api.Group("/auth")
+		{
+			authRoutes.POST("/register", authController.Register)
+			authRoutes.POST("/login", authController.Login)
+		}
+		userRoutes := api.Group("/users").Use(middlewares.JWTAuthMiddleware(cfg.JWTSecret))
+		{
+			userRoutes.GET("/:id", userController.GetUser)
+		}
+		orgRoutes := api.Group("/organisations").Use(middlewares.JWTAuthMiddleware(cfg.JWTSecret))
+		{
+			orgRoutes.GET("/", orgController.GetOrganisations)
+			orgRoutes.GET("/:orgId", orgController.GetOrganisation)
+			orgRoutes.POST("/", orgController.CreateOrganisation)
+			orgRoutes.POST("/:orgId/users", orgController.AddUserToOrganisation)
+		}
+	}
+
+	// Serve the request
+	router.ServeHTTP(w, r)
 }
